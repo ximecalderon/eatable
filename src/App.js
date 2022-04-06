115dcc89fb81b3a569f3ddd897dbf676fff6cf69
Ximena Calderón <86806702+ximecalderon@users.noreply.github.com>
2022-04-06 11:22:21 -0500

    Comments about routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 function App() {
+  //  / => nonauthenticated app maneja login y sgn-up
+  // / => ruta de home muestra productos si estas loggeado
+  // /products/:id => ruta de producto individual
+  // /cart
+  // /cart/checkout
+  // /history
+  // /profile
+
   return (
     <p>Hola</p>
   );
