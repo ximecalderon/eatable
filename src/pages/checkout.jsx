bb6224aef6455978a8a73947beaa8d0a7ed5a707
eduardo4394 <eduardo_sh@outlook.com>
2022-04-06 18:06:38 -0500

    define card structure

---
+++ src/pages/checkout.jsx
@@ -0,0 +1,13 @@
+import CartCard from "../components/card-cart";
+
+
+function CheckoutPage() {
+  return(
+    <section style={{width: "100%"}}>
+      <h1>Cart</h1>
+      <CartCard />
+    </section>
+  )
+}
+
+export default CheckoutPage;
