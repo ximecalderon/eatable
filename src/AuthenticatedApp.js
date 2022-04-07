159f66a635faa2ba68d60afc068df3dc0f391cd3
Marco Antonio <marco_atencio@hotmail.com>
2022-04-07 09:17:39 -0500

    finished merging

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -1,20 +1,18 @@
 import HomePage from './pages/home';
 import ProductDetail from './pages/ProductDetail';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 
 function AuthenticatedApp() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<Layout />}>
-                    <Route path='/' element={<HomePage />} />
-                    <Route path='/products/:id' element={<ProductDetail />} />
-                    <Route path='/history' element={<div>History</div>} />
-                    <Route path='/profile' element={<div>Profile</div>} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/products/:id' element={<ProductDetail />} />
+                <Route path='/history' element={<div>History</div>} />
+                <Route path='/profile' element={<div>Profile</div>} />
+            </Route>
+        </Routes>
     );
 }
 
